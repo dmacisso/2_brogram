4b35f975fbcae1db6bd2763ad64935f6e3c94df7
dmacisso <dmacisso@gmail.com>
2025-02-22 16:56:48 -0500

    add a toggle mode feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from './context/ThemeContext';
 import Grid from './components/Grid';
 import Hero from './components/Hero';
 import Layout from './components/Layout';
-function App() {
+
+const App = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
   return (
     <>
       <Layout>
+      <div>
+          <button onClick={toggleTheme}>Toggle Theme</button>
+          <div>
+            <h1>Current Theme: {theme}</h1>
+          </div>
+        </div>
         <main>
           <Hero />
           <Grid />
@@ -12,6 +23,6 @@ function App() {
       </Layout>
     </>
   );
-}
+};
 
 export default App;
